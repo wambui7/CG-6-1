6f80e37999241bab3352c82f93bc8786ba654efd
wambui7 <shiko.sheila@gmail.com>
2018-08-16 08:32:11 -0500

    no changes

--- stringbuilder/Program.cs
+++ stringbuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace stringbuilder
 {
@@ -7,6 +8,25 @@ namespace stringbuilder
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your first name!");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Enter your last name!");
+            string lastname = Console.ReadLine();
+
+            string fullnames = Combine(firstname, lastname);
+
+            Console.WriteLine(fullnames);
+
+        }
+        private static string Combine(string firstname, string lastname)
+        {
+            StringBuilder fullnames = new StringBuilder();
+
+            fullnames.Append(firstname);
+            fullnames.Append(lastname);
+
+            Console.WriteLine("fullnames");
+            Console.ReadLine();
         }
     }
 }
